f39c719a463abdf7ba39822d564e0bc69f76124b
zamanbhutta <marham.ayat@gmail.com>
2021-02-26 14:28:49 +0100

    exe2 done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,23 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function tellFortune(work, location, partner, children) {
+    this.work = work 
+    this.location = location
+    this.partner = partner
+    this.children = children
+    this.Fortune = function(){
+    window.alert("You will be a " + this.work +" in " + this.location +", and married to " + this.partner + " with " + this.children +" kids.");   
+    }
+}
+
+
+var work = prompt("your work");
+var loc = prompt("where do you want work");
+var partner = prompt("write your partner name ");
+var children = prompt("how many children you have"); 
+     
+
+var zaman = new tellFortune(work, loc, partner, children);
+zaman.Fortune()
