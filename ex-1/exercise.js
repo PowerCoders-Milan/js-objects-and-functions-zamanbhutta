0cf6f51d72b878f5175e56193d6066958c2ae8b2
zamanbhutta <marham.ayat@gmail.com>
2021-02-26 12:37:37 +0100

    exe1 done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,24 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+function person() {
+
+    var noun = prompt("write a noun");
+    var verb = prompt("write a verb");
+    var adverb = prompt("write an adverb ");
+    var adjective = prompt("write an adjective"); 
+    window.alert(noun +" "+ verb +" "+adverb +" "+ adjective); 
+}
+person()
+
+
+
+
+
+
+
+
+
+
