fd7fc0fcf7e376e58f7651aa5709948e07b053a6
zamanbhutta <marham.ayat@gmail.com>
2021-02-26 15:32:47 +0100

    exercise 3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,28 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+myChoices = ["color", "primeMinister", "sport"]
+
+color = {
+    name : "Black",
+    type : "Prime",
+}
+
+primeMinister = {
+   name : "Imran Khan",
+   type : "Tahreekh Insaaf"
+}
+
+sport = {
+    name : "Cricket",
+    type : "played with bat and ball"
+}
+
+window.alert(color.name+ ".")
+window.alert(primeMinister.type+ ".")
+window.alert(sport.name+ ".")
+
+
+
