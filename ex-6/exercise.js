0c4c53440d893cbdcca0e1c24e07f5fa9d2355b9
zamanbhutta <marham.ayat@gmail.com>
2021-02-28 12:41:17 +0100

    exercise 6

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,3 +1,16 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function greaterNum(num1, num2) {
+
+    if(num1 > num2){
+        window.alert("The greater number of " + num1 + " and " + num2 + " is " + num1)
+    }else if(num2 < num1){
+        window.alert("The greater number of " + num2 + " and " + num1 + " is " + num2)
+    }else{
+        window.alert("The number are the same")
+    }
+}
+
+greaterNum(10, 5)
+greaterNum(250,100)
